433c13884547da7cf76a13f54dd0bbc75c305166
Ushie01 <abugodwinaj@gmail.com>
2023-11-14 18:30:32 +0100

    create: finishing of portfolio

--- src/client/components/portfolio/RecentWorks/RecentWorks.tsx
+++ src/client/components/portfolio/RecentWorks/RecentWorks.tsx
@@ -10,15 +10,17 @@ const RecentWorks = () => {
 				<p className='text-white text-6xl font-bold pb-16'>Recent works</p>
 				<div className='grid grid-cols-2 gap-8 gap-x-24 gap-y-24 items-center justify-between '>
 					{CARD_DATA.map((value, index) => (
-						<Card
-							index={index}
-							link={value.link}
-							img={value.img}
-							frame={value.frame}
-							workTitle={value.workTitle}
-							projectType={value.projectType}
-							viewWebsite={value.viewWebsite}
-						/>
+						<div key={index}>
+							<Card
+								index={index}
+								link={value.link}
+								img={value.img}
+								frame={value.frame}
+								workTitle={value.workTitle}
+								projectType={value.projectType}
+								viewWebsite={value.viewWebsite}
+							/>
+						</div>
 					))}
 				</div>
 			</div>
